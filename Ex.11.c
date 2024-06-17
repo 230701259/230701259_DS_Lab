ff5a96c6a081206b36ea2339fa79ec01022cc654
Rama Thulasi G <230701259@rajalakshmi.edu.in>
2024-06-17 19:30:26 +0530

    Ex.11.c

--- Ex.11.c
+++ Ex.11.c
@@ -1,108 +1,155 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define MAX 100
-struct Node {
- int vertex;
- struct Node* next;
-};
-struct Node* createNode(int v);
-struct Graph {
- int numVertices;
- struct Node** adjLists;
- int* visited;
-};
-struct Graph* createGraph(int vertices);
-void addEdge(struct Graph* graph, int src, int dest);
-void printGraph(struct Graph* graph);
-void BFS(struct Graph* graph, int startVertex);
-void DFS(struct Graph* graph, int startVertex);
-void DFSUtil(struct Graph* graph, int vertex);
-int main() {
- struct Graph* graph = createGraph(4);
- addEdge(graph, 0, 1);
- addEdge(graph, 0, 2);
- addEdge(graph, 1, 2);
- addEdge(graph, 2, 0);
- addEdge(graph, 2, 3);
- addEdge(graph, 3, 3);
- printf("Graph:\n");
- printGraph(graph);
- printf("\nBFS Traversal:\n");
- BFS(graph, 2);
- printf("\nDFS Traversal:\n");
- DFS(graph, 2);
- return 0;
+#include<stdio.h>
+
+int q[20],top=-1,front=-1,rear=-1,a[20][20],vis[20],stack[20];
+int delete();
+void add(int item);
+void bfs(int s,int n);
+void dfs(int s,int n);
+void push(int item);
+int pop();
+
+void main()
+{
+int n,i,s,ch,j;
+char c,dummy;
+printf("ENTER THE NUMBER VERTICES ");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=n;j++)
+{
+printf("ENTER 1 IF %d HAS A NODE WITH %d ELSE 0 ",i,j);
+scanf("%d",&a[i][j]);
 }
-struct Node* createNode(int v) {
- struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
- newNode->vertex = v;
- newNode->next = NULL;
- return newNode;
 }
-struct Graph* createGraph(int vertices) {
- struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
- graph->numVertices = vertices;
- graph->adjLists = (struct Node**)malloc(vertices * sizeof(struct Node*));
- graph->visited = (int*)malloc(vertices * sizeof(int));
- for (int i = 0; i < vertices; i++) {
- graph->adjLists[i] = NULL;
- graph->visited[i] = 0;
- }
- return graph;
+printf("THE ADJACENCY MATRIX IS\n");
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=n;j++)
+{
+printf(" %d",a[i][j]);
 }
-void addEdge(struct Graph* graph, int src, int dest) {
- struct Node* newNode = createNode(dest);
- newNode->next = graph->adjLists[src];
- graph->adjLists[src] = newNode;
- newNode = createNode(src);
- newNode->next = graph->adjLists[dest];
- graph->adjLists[dest] = newNode;
+printf("\n");
 }
-void printGraph(struct Graph* graph) {
- for (int v = 0; v < graph->numVertices; v++) {
- struct Node* temp = graph->adjLists[v];
- printf("Vertex %d: ", v);
- while (temp) {
- printf("%d -> ", temp->vertex);
- temp = temp->next;
- }
- printf("NULL\n");
- }
+
+do
+{
+for(i=1;i<=n;i++)
+vis[i]=0;
+printf("\nMENU");
+printf("\n1.B.F.S");
+printf("\n2.D.F.S");
+printf("\nENTER YOUR CHOICE");
+scanf("%d",&ch);
+printf("ENTER THE SOURCE VERTEX :");
+scanf("%d",&s);
+
+switch(ch)
+{
+case 1:bfs(s,n);
+break;
+case 2:
+dfs(s,n);
+break;
 }
-void BFS(struct Graph* graph, int startVertex) {
- int queue[MAX];
- int front = 0, rear = 0;
- graph->visited[startVertex] = 1;
- queue[rear++] = startVertex;
- while (front < rear) {
- int currentVertex = queue[front++];
- printf("Visited %d\n", currentVertex);
- struct Node* temp = graph->adjLists[currentVertex];
- while (temp) {
- int adjVertex = temp->vertex;
- if (graph->visited[adjVertex] == 0) {
- graph->visited[adjVertex] = 1;
- queue[rear++] = adjVertex;
- }
- temp = temp->next;
- }
- }
+printf("DO U WANT TO CONTINUE(Y/N) ? ");
+scanf("%c",&dummy);
+scanf("%c",&c);
+}while((c=='y')||(c=='Y'));
 }
-void DFSUtil(struct Graph* graph, int vertex) {
- struct Node* temp = graph->adjLists[vertex];
- graph->visited[vertex] = 1;
- printf("Visited %d\n", vertex);
- while (temp) {
- int adjVertex = temp->vertex;
- if (graph->visited[adjVertex] == 0) {
- DFSUtil(graph, adjVertex);
- }
- temp = temp->next;
- }
+
+void bfs(int s,int n)
+{
+int p,i;
+add(s);
+vis[s]=1;
+p=delete();
+if(p!=0)
+printf(" %d",p);
+while(p!=0)
+{
+for(i=1;i<=n;i++)
+if((a[p][i]!=0)&&(vis[i]==0))
+{
+add(i);
+vis[i]=1;
 }
-void DFS(struct Graph* graph, int startVertex) {
- for (int i = 0; i < graph->numVertices; i++) {
- graph->visited[i] = 0;
- }
- DFSUtil(graph, startVertex);
+p=delete();
+if(p!=0)
+printf(" %d ",p);
+}
+for(i=1;i<=n;i++)
+if(vis[i]==0)
+bfs(i,n);
+}
+
+
+void add(int item)
+{
+if(rear==19)
+printf("QUEUE FULL");
+else
+{
+if(rear==-1)
+{
+q[++rear]=item;
+front++;
+}
+else
+q[++rear]=item;
+}
+}
+int delete()
+{
+int k;
+if((front>rear)||(front==-1))
+return(0);
+else
+{
+k=q[front++];
+return(k);
+}
+}
+
+void dfs(int s,int n)
+{
+int i,k;
+push(s);
+vis[s]=1;
+k=pop();
+if(k!=0)
+printf(" %d ",k);
+while(k!=0)
+{
+for(i=1;i<=n;i++)
+if((a[k][i]!=0)&&(vis[i]==0))
+{
+push(i);
+vis[i]=1;
+}
+k=pop();
+if(k!=0)
+printf(" %d ",k);
+}
+for(i=1;i<=n;i++)
+if(vis[i]==0)
+dfs(i,n);
+}
+void push(int item)
+{
+if(top==19)
+printf("Stack overflow ");
+else
+stack[++top]=item;
+}
+int pop()
+{
+int k;
+if(top==-1)
+return(0);
+else
+{
+k=stack[top--];
+return(k);
+}
 }
